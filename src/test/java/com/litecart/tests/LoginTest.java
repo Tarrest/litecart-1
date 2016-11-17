b53ecdd1a25a3833a315861dc306ba875f3a07bb
pshynin <pshynin@yahoo.com>
2016-11-16 22:37:08 -0500

    Initial commit

---
+++ src/test/java/com/litecart/tests/LoginTest.java
@@ -0,0 +1,12 @@
+package com.litecart.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest extends TestBase {
+
+
+    @Test(enabled = true)
+    public void testStartBrowserLogin() {
+       // search("selenium");
+    }
+}
---
+++ src/test/java/com/litecart/tests/TestBase.java
@@ -0,0 +1,76 @@
+package com.litecart.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    FirefoxDriver wd;
+
+    Logger logger = LoggerFactory.getLogger(LoginTest.class);
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("https://www.google.com/");
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        wd.quit();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName());
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m, Object[] p) {
+        logger.info("Stop test " + m.getName());
+    }
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
+        if (text != null) {
+            String existingText = wd.findElement(locator).getAttribute("value");
+            if (!text.equals(existingText)) {
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+        }
+    }
+
+    public boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    boolean isElementPresent(By locator) {
+        try {
+            wd.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+    }
+}
