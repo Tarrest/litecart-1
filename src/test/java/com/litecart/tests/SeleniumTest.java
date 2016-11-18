04d2d2257fbb09144637dd882a0f466156f01aca
pshynin <pshynin@yahoo.com>
2016-11-17 21:44:00 -0500

    'добавлены тесты и проверки'

--- src/test/java/com/litecart/tests/SeleniumTest.java
+++ src/test/java/com/litecart/tests/SeleniumTest.java
@@ -1,6 +1,7 @@
 package com.litecart.tests;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -9,9 +10,18 @@ import org.testng.annotations.Test;
 public class SeleniumTest extends TestBase {
 
     @Test(enabled = true)
-    public void test1() {
-        navigateTo("http://www.google.com");
-        type(By.name("q"), "webdriver");
+    public void navigateTest() {
+        navigateTo("https://www.google.com/");
+        String url = wd.getCurrentUrl();
+        Assert.assertEquals(url, "https://www.google.com/");
+    }
+
+    @Test(enabled = true)
+    public void searchTest() {
+        navigateTo("https://www.google.com/?gws_rd=ssl");
+        type(By.name("q"), "Google");
         click(By.name("btnG"));
+        String title = wd.getTitle();
+        Assert.assertEquals(title, "Google");
     }
 }
