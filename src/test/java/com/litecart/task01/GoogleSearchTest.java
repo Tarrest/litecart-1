a119b4aa642e7c71fc1fcd440ebef18042d62031
pshynin <pshynin@yahoo.com>
2016-12-13 23:08:04 -0500

    Task 1 done

--- src/test/java/com/litecart/task01/GoogleSearchTest.java
+++ src/test/java/com/litecart/task01/GoogleSearchTest.java
@@ -1,7 +1,6 @@
 package com.litecart.task01;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -15,10 +14,14 @@ import org.testng.annotations.Test;
  * Created by pshynin on 11/17/16.
  */
 public class GoogleSearchTest {
+    private static final String URL = "http://google.com";
+    private static final String FIELD = "#lst-ib";
+    private static final String BUTTON = "#_fZl";
+    private static final String RESULT = "div.f";
+    private static final String SEARCH = "selenium";
+
     private static final int SLEEP_PERIOD = 1000;
     private static final int TIMEOUT = 30000;
-    private static final String SEARCH_STR = "selenium";
-    private static final String URL = "http://google.com";
 
     private WebDriver driver;
     private WebDriverWait wait;
@@ -42,36 +45,10 @@ public class GoogleSearchTest {
     @Test(enabled = true)
     public void test1() {
         this.driver.get(URL);
-        this.driver.findElement(By.id("lst-ib"))
-                .sendKeys(GoogleSearchTest.SEARCH_STR);
-        this.driver.findElement(By.name("btnG")).click();
-        By result = By.className("g");
-        this.wait.until(ExpectedConditions.presenceOfElementLocated(result));
-        Assert.assertTrue(this.driver.findElement(result).getText()
-                        .contains(GoogleSearchTest.SEARCH_STR),
-                "Search results do not contain search string");
-    }
-
-    @Test(enabled = true)
-    public void test2() {
-        this.driver.navigate().to(URL);
-        this.driver.findElement(By.name("q")).sendKeys("webdriver");
-        Assert.assertFalse(isElementPresent(By.name("XXX")));
-    }
-
-    @Test(enabled = true)
-    public void test3() {
-        this.driver.navigate().to("http://www.google.com");
-        this.driver.findElement(By.name("q")).sendKeys("webdriver");
-        Assert.assertFalse(this.isElementPresent(By.xpath("//div[")));
-    }
-
-    private boolean isElementPresent(By locator) {
-        try {
-            this.driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException ex) {
-            return false;
-        }
+        this.driver.findElement(By.cssSelector(FIELD)).sendKeys(SEARCH);
+        this.driver.findElement(By.cssSelector(BUTTON)).click();
+        this.wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(RESULT)));
+        Assert.assertTrue(this.driver.findElement(By.cssSelector(RESULT))
+                .getText().contains(SEARCH), "Search results not found");
     }
 }
