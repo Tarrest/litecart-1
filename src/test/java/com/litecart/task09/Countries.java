cd4e2b2575d22669a57f56413aa31398235b36eb
pshynin <pshynin@yahoo.com>
2016-12-18 21:37:27 -0500

    Task 9 done

--- src/test/java/com/litecart/task09/Countries.java
+++ src/test/java/com/litecart/task09/Countries.java
@@ -16,13 +16,17 @@ import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
-public class GeoZonesSortTest {
+/**
+ * Created by pshynin on 12/16/16.
+ */
+public class Countries {
     private static final int SLEEP_PERIOD = 1000;
     private static final int TIMEOUT = 30000;
     private static final String URL_APP = "http://localhost/litecart/admin";
-    private final static String URL_GEO_ZONE = "http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones";
-    private final static String COUNTRY = "//form[@name='geo_zones_form']//td[3]/a";
-    private final static String COUNTRY_ZONE = "//*[@id='table-zones']//td[3]/select/option[@selected='selected']";
+    private final static String URL_COUNTRIES = "http://localhost/litecart/admin/?app=countries&doc=countries";
+    private final static String COUNTRY = "//form[@name='countries_form']//td[5]/a";
+    private final static String COUNTRY_ZONE = "//form[@name='countries_form']//td[6]";
+    private final static String ZONE_ROW = "//*[@id='table-zones']//tr/td[3]";
 
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "admin";
@@ -33,36 +37,54 @@ public class GeoZonesSortTest {
     @BeforeTest
     public void beforeTest() {
         this.driver = new FirefoxDriver();
-        this.wait = new WebDriverWait(this.driver, GeoZonesSortTest.TIMEOUT,
-                GeoZonesSortTest.SLEEP_PERIOD);
+        this.wait = new WebDriverWait(this.driver, Countries.TIMEOUT,
+                Countries.SLEEP_PERIOD);
         this.driver.get(URL_APP);
         login(USERNAME, PASSWORD);
     }
 
-    @AfterTest(alwaysRun = true)
+    @AfterTest (alwaysRun = true)
     public void afterTest() {
         this.driver.quit();
     }
 
-    @Test
-    public void testGeoZonesOrder() {
-        driver.get(URL_GEO_ZONE);
-        List<WebElement> countries = driver.findElements(By.xpath(COUNTRY));
+    @Test(enabled = true)
+    public void testCountriesOrder() {
+        this.driver.get(URL_COUNTRIES);
+        List<WebElement> countries = this.driver.findElements(By.xpath(COUNTRY));
+        List<String> before = new ArrayList<>();
+        List<String> after = new ArrayList<>();
+
         for (WebElement country : countries) {
-            country.click();
-
-            List<WebElement> zones = driver.findElements(By.xpath(COUNTRY_ZONE));
-            List<String> before = new ArrayList<>();
-            List<String> after = new ArrayList<>();
-
-            for (WebElement zone : zones) {
-                before.add(zone.getText());
-                after.add(zone.getText());
-            }
+            before.add(country.getText());
+            after.add(country.getText());
 
             Collections.sort(after);
             assertEquals(after, before);
         }
+    }
+
+    @Test(enabled = true)
+    public void testCountryZonesOrder() {
+        this.driver.get(URL_COUNTRIES);
+        List<WebElement> countryZones = driver.findElements(By.xpath(COUNTRY_ZONE));
+        countryZones.stream().filter(zone -> Integer.parseInt(zone.getText()) != 0).forEach(zone -> {
+            zone.click();
+
+            List<WebElement> zoneRows = driver.findElements(By.cssSelector(ZONE_ROW));
+            zoneRows.remove(zoneRows.size() - 1);
+
+            List<String> before = new ArrayList<>();
+            List<String> after = new ArrayList<>();
+
+            for (WebElement row : zoneRows) {
+                before.add(row.getText());
+                after.add(row.getText());
+            }
+
+            Collections.sort(after);
+            assertEquals(after, before);
+        });
     }
 
     private void login(String username, String password) {
