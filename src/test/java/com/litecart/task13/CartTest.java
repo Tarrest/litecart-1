b79ef4c0f1d6acd92b98c2824553e7eceb703965
pshynin <pshynin@yahoo.com>
2016-12-20 21:39:32 -0500

    Task 13 done

--- src/test/java/com/litecart/task13/CartTest.java
+++ src/test/java/com/litecart/task13/CartTest.java
@@ -1,10 +1,19 @@
 package com.litecart.task13;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by pshynin on 12/18/16.
@@ -13,72 +22,84 @@ public class CartTest {
     private static final int SLEEP_PERIOD = 1000;
     private static final int TIMEOUT = 30000;
     private static final String URL = "http://localhost/litecart";
-    private static final String PRODUCT = "";
 
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @BeforeTest
     public void beforeTest() {
         this.driver = new FirefoxDriver();
-        this.wait = new WebDriverWait(this.driver, CartTest.TIMEOUT,
+        new WebDriverWait(this.driver, CartTest.TIMEOUT,
                 CartTest.SLEEP_PERIOD);
         this.driver.get(URL);
     }
 
-    @AfterTest
+    @AfterTest(alwaysRun = true)
     public void afterTest() {
         this.driver.quit();
     }
 
-    @Test
+    @Test(enabled = true)
     public void testProductAdd() {
-        selectProduct();
-        addToCart();
+        if (quantity() < 3) {
+            int before = quantity();
+            homePage();
+            selectProduct();
+            addToCart();
+            int after = quantity();
 
-        verifyProductAdd();
-
+            assertEquals(before, after - 1);
+        }
     }
 
-    @Test
+    @Test(enabled = false)
     public void testProductRemove() {
-        checkOut();
-        removeFromCart();
+        while (quantity() != 0) {
+            int before = quantity();
+            checkOut();
+            removeFromCart();
+            int after = quantity();
 
-        verifyProductRemove();
+            assertEquals(before, after + 1);
+        }
     }
 
     private void selectProduct() {
+        List<WebElement> products = driver.findElements(By.cssSelector("li.product"));
+        for (WebElement product : products) {
+            if (isElementPresent(By.cssSelector("[name='options[Size]']"))) {
+                new Select(this.driver.findElement(By.cssSelector("option.value")));
+            }
+            product.click();
+        }
+    }
+
+    private void homePage() {
+        this.driver.findElement(By.cssSelector("[id=logotype-wrapper]")).click();
     }
 
     private void addToCart() {
+        this.driver.findElement(By.cssSelector("[name=add_cart_product]")).click();
     }
 
     private void removeFromCart() {
+        this.driver.findElement(By.cssSelector("[name='remove_cart_item']")).click();
     }
 
     private void checkOut() {
+        this.driver.findElement(By.cssSelector("#cart .link")).click();
     }
 
-    private void verifyProductAdd() {
-        checkOut();
+    private int quantity() {
+        String quantity = this.driver.findElement(By.cssSelector("#cart .quantity")).getText();
+        return Integer.parseInt(quantity);
     }
 
-    private void verifyProductRemove() {
-        checkOut();
+    private boolean isElementPresent(By locator) {
+        try {
+            this.driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
     }
-
-
-
-
-//    Сделайте сценарий для добавления товаров в корзину и удаления товаров из корзины.
-//    Сценарий должен состоять из следующих частей:
-//              1) открыть страницу какого-нибудь товара
-//              2) добавить его в корзину
-//              3) подождать, пока счётчик товаров в корзине обновится
-//              4) вернуться на главную страницу, повторить предыдущие шаги ещё два раза,
-//                  чтобы в общей сложности в корзине было 3 единицы товара
-//              5) открыть корзину (в правом верхнем углу кликнуть по ссылке Checkout)
-//              6) удалить все товары из корзины один за другим, после каждого удаления подождать, пока внизу обновится таблица
-
 }
